51f8b48009cc2825fb1b09358b711f41e33f85b7
Tiago Caceraghi dos Santos <tiago.caceraghi@edu.sc.senai.br>
2022-11-09 21:08:17 -0300

    Versão 0.0.0.1 - Projeto Base

---
+++ adms/index.php
@@ -0,0 +1,17 @@
+<?php
+    if(!isset($_SESSION['email']) && !isset($_SESSION['senha'])):
+        header("Location:login.php");
+    endif;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Painel Administrativo</title>
+</head>
+<body>
+    <h1>Painel Administrativo</h1>  
+</body>
+</html>
---
+++ adms/login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login de Sistema</title>
+</head>
+<body>
+    <h3>Login de Sistema</h3>
+    <form>
+        <label>E-mail:<input type="text" name="email"></label><br><br>
+        <label>Senha:<input type="password" name="senha"></label><br><br>
+        <input type="submit" value="Login">
+</form>
+</body>
+</html>
